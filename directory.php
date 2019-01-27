b98e26d537a17f860c2b54c17d61dff2c1923299
Amanda Maring <amaring@users.noreply.github.com>
2019-01-27 15:28:03 -0500

    Starting files

---
+++ directory.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf8">
+	<title></title>
+	<meta name="description" content="">
+	<meta name="author" content="Amanda Maring">
+	<link rel="stylesheet" href="normalize.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<aside class="leftnav">
+		<h2>Offices</h2>
+		<h3>Grand Rapids</h3>
+		<h3>Kalamazoo</h3>
+		<h3>Lansing</h3>
+		<h3>Marquette</h3>
+		<h2>Chambers</h2>
+		<h3>Judge David W. McKeague</h3>
+		<h3>Judge Richard A. Griffin</h3>
+		<h3>Judge Richard F. Suhrheinrich</h3>
+		<h3>Chief Judge Robert J. Jonker</h3>
+		<h3>Judge Paul L. Maloney</h3>
+		<h3>Judge Janet T. Neff</h3>
+		<h3>Judge Gordon J. Quist</h3>
+		<h3>Magistrate Judge Timothy P. Greeley</h3>
+		<h3>Magistrate Judge Ellen S. Carmody</h3>
+		<h3>Magistrate Judge Phillip J. Green</h3>
+		<h3>Magistrate Judge Ray Kent</h3>
+		<h2>Departments</h2>
+		<h3>Pro Se</h3>
+		<h3>Clerks</h3>
+		<h3>Finance</h3>
+		<h3>Information Technology</h3>
+		<h3>Library</h3>
+		<h3>Probation</h3>
+		<h3>Other</h3>
+	</aside>
+	<header>
+		<form action="" method="POST" class="search" autocomplete="off">
+			<input type="text" name="term" id="term" placeholder="Find someone...">
+			<input type="submit" value="Search">
+		</form>
+		<ul class="alpha">
+			<li>A</li>
+			<li>B</li>
+			<li>C</li>
+			<li>D</li>
+			<li>E</li>
+			<li>F</li>
+			<li>G</li>
+			<li>H</li>
+			<li>I</li>
+			<li>J</li>
+			<li>K</li>
+			<li>L</li>
+			<li>M</li>
+			<li>N</li>
+			<li>O</li>
+			<li>P</li>
+			<li>Q</li>
+			<li>R</li>
+			<li>S</li>
+			<li>T</li>
+			<li>U</li>
+			<li>V</li>
+			<li>W</li>
+			<li>X</li>
+			<li>Y</li>
+			<li>Z</li>
+		</ul>
+	</header>
+	<section class="employee">
+	<ul class="emplist">
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">Luke Beasley</span>
+			<span class="title">Law Clerk</span>
+		</li>
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">Paul Brandenburg</span>
+			<span class="title">Law Clerk</span>
+		</li>
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">Andrew Clopton</span>
+			<span class="title">Law Clerk</span>
+		</li>
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">Dorothy Gordon</span>
+			<span class="title">Judicial Assistant</span>
+		</li>
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">Christopher Kozak</span>
+			<span class="title">Law Clerk</span>
+		</li>
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">First Last</span>
+			<span class="title">Title</span>
+		</li>
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">First Last</span>
+			<span class="title">Title</span>
+		</li>
+		<li>
+			<img src="WDMI_seal.png">
+			<span class="name">First Last</span>
+			<span class="title">Title</span>
+		</li>
+
+	</ul>
+	</section>
+</body>
+</html>
